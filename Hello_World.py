73977b394f609013694a77832746a9f83197ec2f
jcambray10 <84952483+jcambray10@users.noreply.github.com>
2021-08-07 16:11:53 -0500

    first commit

---
+++ Hello_World.py
@@ -0,0 +1,36 @@
+# print("Hello World!")
+# x = "Hello Python"
+# print(x)
+# y = 42
+# print(y)
+
+# 1. TASK: print "Hello World"
+# print("Hello World")
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Jose"
+print("Hello", name)	# with a comma
+print("Hello " + name)	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name = 42
+print("Hello", name)	# with a comma
+print("Hello "+ str(name))	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print(f"My name is {first_name} {last_name} and I am {age} years old.")
+
+
+# fave_food1 = "sushi"
+# fave_food2 = "pizza"
+# print("I love to eat {} and {}".format(fave_food1, fave_food2)) # with .format()
+# print(f"I love to eat {fave_food1}.")
+
+# echo "# Hello_World" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/jcambray10/Hello_World.git
+# git push -u origin main
+
